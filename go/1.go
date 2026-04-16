f27d7e334d7d0ec4abeab2f24267b6146aefcfe6
GuilhermeTavares4 <guideb23@gmail.com>
2026-04-16 15:05:06 -0300

    exercicios de go

---
+++ go/1.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+import "math"
+
+func main() {
+    var x, n float64
+    fmt.Print("Digite a base: ")
+    fmt.Scan(&x)
+    fmt.Print("Digite o expoente: ")
+    fmt.Scan(&n)
+    result := math.Pow(x, n)
+    fmt.Printf("O resultado é: %v.", result)
+}
---
+++ go/2.go
@@ -0,0 +1,25 @@
+package main
+import "fmt"
+import "math"
+
+func main() {
+    var x int
+    var is_primo bool = true
+    fmt.Print("Digite um valor: ")
+    fmt.Scan(&x)
+    for i := 2.0; i <= math.Floor(float64 (x) / 2); i++{
+        if math.Mod(float64 (x), i) == 0 {
+            is_primo = false
+            break
+        }
+    }
+    if x == 1{
+        is_primo = false
+    }
+    if is_primo {
+        fmt.Printf("O número %v é primo.", x)
+    } else {
+        fmt.Printf("O número %v não é primo.", x)
+    }
+
+}
